43042cac851afa76c1ad3a4884eb285254751af7
Daniel Weinman <namniewd@gmail.com>
2018-05-13 13:32:14 -0400

    fixed code style for pep8

--- fortnite_catalog.py
+++ fortnite_catalog.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-app = Flask(__name__)
+from flask import (
+	Flask, render_template, request, redirect, url_for, flash, jsonify)
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from fortnite_database_setup import Base, User, Weapon, WeaponDetail
 
 
 from flask import session as login_session
-import random, string
+import random
+import string
 
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
@@ -15,6 +18,8 @@ import json
 from flask import make_response
 import requests
 
+app = Flask(__name__)
+
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Fortnite Catalog"
@@ -22,18 +27,23 @@ APPLICATION_NAME = "Fortnite Catalog"
 engine = create_engine('sqlite:///fortniteweapondatabase.db')
 Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind = engine)
+DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-#Create anti-forgery state token
+# Create anti-forgery state token
+
+
 @app.route('/login')
 def showLogin():
-	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+	state = ''.join(random.choice(
+		string.ascii_uppercase + string.digits) for x in xrange(32))
 	login_session['state'] = state
 	return render_template('login.html', STATE=state)
 
 
-#Gconnect method for google sign-in
+#  Gconnect method for google sign-in
+
+
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     # Validate state token
@@ -86,8 +96,8 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(json.dumps(
+        	'Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -106,10 +116,10 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
-    #see if user exists, if it does not make a new one
+    # see if user exists, if it does not make a new one
     user_id = getUserID(login_session['email'])
     if not user_id:
-    	user_id = createUser(login_session)
+		user_id = createUser(login_session)
     login_session['user_id'] = user_id
 
     output = ''
@@ -118,13 +128,17 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;border-radius:'
+    output += ' 150px;-webkit-border-radius:'
+    output += ' 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
 
 
-#User Helper Functions
+#  User Helper Functions
+
+
 def createUser(login_session):
     newUser = User(username=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
@@ -147,19 +161,24 @@ def getUserID(email):
         return None
 
 
-#Route for GDisconnect to disconnect from login session.  Revoke's user token and reset login_session.
+# Route for GDisconnect to disconnect from login session.
+# Revoke's user token and reset login_session.
+
+
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
         print 'Access Token is None'
-        response = make_response(json.dumps('Current user not connected.'), 401)
+        response = make_response(json.dumps(
+        	'Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     print 'In gdisconnect access token is %s', access_token
     print 'User name is: '
     print login_session['username']
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    url = ('https://accounts.google.com/o/oauth2/' +
+    	'revoke?token=%s' % login_session['access_token'])
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     print 'result is '
@@ -174,113 +193,139 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return redirect(url_for('showWeapons'))
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response = make_response(
+        	json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
 
 
-#Route for root of site to show all weapon classes
+# Route for root of site to show all weapon classes
+
+
 @app.route('/')
 @app.route('/fortnite')
 def showWeapons():
 	weapons = session.query(Weapon).all()
 	if 'username' not in login_session:
-		return render_template('publicfortnite.html', weapons = weapons)
+		return render_template('publicfortnite.html', weapons=weapons)
 	else:
-		return render_template('fortnite.html', weapons = weapons)
+		return render_template('fortnite.html', weapons=weapons)
 
-#Route to add new weapon
-@app.route('/fortnite/new', methods = ['GET', 'POST'])
+# Route to add new weapon
+
+
+@app.route('/fortnite/new', methods=['GET', 'POST'])
 def newWeapon():
 	if 'username' not in login_session:
 		return redirect('/login')
 	if request.method == 'POST':
 		if request.form['name']:
-			newWeapon = Weapon(name = request.form['name'], user_id = login_session['user_id'])
+			newWeapon = Weapon(
+				name=request.form['name'], user_id=login_session['user_id'])
 			session.add(newWeapon)
 			session.commit()
 		return redirect(url_for('showWeapons'))
 	return render_template('newWeapon.html')
 
 
-#Route to edit existing weapon
-@app.route('/fortnite/<string:weapon_name>/edit', methods = ['GET', 'POST'])
+# Route to edit existing weapon
+
+
+@app.route('/fortnite/<string:weapon_name>/edit', methods=['GET', 'POST'])
 def editWeapon(weapon_name):
 	if 'username' not in login_session:
 		return redirect('/login')
-	editedWeapon = session.query(Weapon).filter_by(name = weapon_name).one()
+	editedWeapon = session.query(Weapon).filter_by(name=weapon_name).one()
 	if editedWeapon.user_id != login_session['user_id']:
-		return "<script>function myFunction() {alert('You are not authorized to edit this weapon.  Are you its creator?');}</script><body onload='myFunction()''>"
+		return "<script>function myFunction() {alert('You are not authorized to edit this weapon.  Are you its creator?');}</script><body onload='myFunction()''>"  # noqa
 	if request.method == 'POST':
 		if request.form['name']:
 			editedWeapon.name = request.form['name']
 			session.add(editedWeapon)
 			session.commit()
 		return redirect(url_for('showWeapons'))
-	return render_template('editWeapon.html', weapon = editedWeapon)
+	return render_template('editWeapon.html', weapon=editedWeapon)
 
 
-#Route to delete existing weapon
-@app.route('/fortnite/<string:weapon_name>/delete', methods = ['GET', 'POST'])
+# Route to delete existing weapon
+
+
+@app.route('/fortnite/<string:weapon_name>/delete', methods=['GET', 'POST'])
 def deleteWeapon(weapon_name):
 	if 'username' not in login_session:
 		return redirect('/login')
-	deletedWeapon = session.query(Weapon).filter_by(name = weapon_name).one()
+	deletedWeapon = session.query(Weapon).filter_by(name=weapon_name).one()
 	if deletedWeapon.user_id != login_session['user_id']:
-		return "<script>function myFunction() {alert('You are not authorized to delete this weapon.  Are you its creator?);}</script><body onload='myFunction()''>"
+		return "<script>function myFunction() {alert('You are not authorized to delete this weapon.  Are you its creator?);}</script><body onload='myFunction()''>"  # noqa
 	if request.method == 'POST':
 		session.delete(deletedWeapon)
 		session.commit()
 		return redirect(url_for('showWeapons'))
-	return render_template('deleteWeapon.html', weapon = deletedWeapon)
+	return render_template('deleteWeapon.html', weapon=deletedWeapon)
 
 
-#Route to show list of all types of a weapon
+# Route to show list of all types of a weapon
+
+
 @app.route('/fortnite/<string:weapon_name>/list')
 def showWeaponList(weapon_name):
-	weapon = session.query(Weapon).filter_by(name = weapon_name).one()
-	items = session.query(WeaponDetail).filter_by(weapon_id = weapon.id)
+	weapon = session.query(Weapon).filter_by(name=weapon_name).one()
+	items = session.query(WeaponDetail).filter_by(weapon_id=weapon.id)
 	if 'username' not in login_session:
-		return render_template('publicWeaponList.html', weapon = weapon, items = items)
+		return render_template('publicWeaponList.html', weapon=weapon, items=items)
 	else:
-		return render_template('weaponList.html', weapon = weapon, items = items)
+		return render_template('weaponList.html', weapon=weapon, items=items)
 
-#Route to show a specific weapon detail
+# Route to show a specific weapon detail
+
+
 @app.route('/fortnite/<string:weapon_name>/list/<string:weapon_detail_name>')
 def showWeaponDetail(weapon_name, weapon_detail_name):
-	weapon = session.query(Weapon).filter_by(name = weapon_name).one()
-	weapon_detail = session.query(WeaponDetail).filter_by(name = weapon_detail_name).one()
+	weapon = session.query(Weapon).filter_by(name=weapon_name).one()
+	weapon_detail = session.query(
+		WeaponDetail).filter_by(name=weapon_detail_name).one()
 	if 'username' not in login_session:
-		return render_template('publicWeaponDetail.html', weapon = weapon, weapon_detail = weapon_detail)
+		return render_template(
+			'publicWeaponDetail.html', weapon=weapon, weapon_detail=weapon_detail)
 	else:
-		return render_template('weaponDetail.html', weapon = weapon, weapon_detail = weapon_detail)
+		return render_template(
+			'weaponDetail.html', weapon=weapon, weapon_detail=weapon_detail)
 
 
+# Route to add weapon detail
 
-#Route to add weapon detail
-@app.route('/fortnite/<string:weapon_name>/list/new', methods = ['GET', 'POST'])
+
+@app.route('/fortnite/<string:weapon_name>/list/new', methods=['GET', 'POST'])
 def newWeaponDetail(weapon_name):
 	if 'username' not in login_session:
 		return redirect('/login')
-	weapon = session.query(Weapon).filter_by(name = weapon_name).one()
+	weapon = session.query(Weapon).filter_by(name=weapon_name).one()
 	if request.method == 'POST':
-		new_weapon_detail = WeaponDetail(name = request.form['name'], description = request.form['description'], color = request.form['color'], damage = request.form['damage'], weapon_id = weapon.id, user_id = login_session['user_id'])
+		new_weapon_detail = WeaponDetail(
+			name=request.form['name'],
+			description=request.form['description'],
+			color=request.form['color'],
+			damage=request.form['damage'],
+			weapon_id=weapon.id,
+			user_id=login_session['user_id'])
 		session.add(new_weapon_detail)
 		session.commit()
-		return redirect(url_for('showWeaponList', weapon_name = weapon.name))
-	return render_template('newWeaponDetail.html', weapon_name = weapon_name)
+		return redirect(url_for('showWeaponList', weapon_name=weapon.name))
+	return render_template('newWeaponDetail.html', weapon_name=weapon_name)
 
 
+# Route to edit weapon detail
 
-#Route to edit weapon detail
-@app.route('/fortnite/<string:weapon_name>/list/<string:weapon_detail_name>/edit', methods = ['GET', 'POST'])
+
+@app.route('/fortnite/<string:weapon_name>/list/<string:weapon_detail_name>/edit', methods=['GET', 'POST'])  # noqa
 def editWeaponDetail(weapon_name, weapon_detail_name):
 	if 'username' not in login_session:
 		return redirect('/login')
-	weapon = session.query(Weapon).filter_by(name = weapon_name).one()
-	editedWeaponDetail = session.query(WeaponDetail).filter_by(name = weapon_detail_name).one()
+	weapon = session.query(Weapon).filter_by(name=weapon_name).one()
+	editedWeaponDetail = session.query(WeaponDetail).filter_by(
+		name=weapon_detail_name).one()
 	if editedWeaponDetail.user_id != login_session['user_id']:
-		return "<script>function myFunction() {alert('You are not authorized to edit this weapon.  Are you its creator?');}</script><body onload='myFunction()''>"
+		return "<script>function myFunction() {alert('You are not authorized to edit this weapon.  Are you its creator?');}</script><body onload='myFunction()''>"  # noqa
 	if request.method == 'POST':
 		if request.form['name']:
 			editedWeaponDetail.name = request.form['name']
@@ -292,49 +337,64 @@ def editWeaponDetail(weapon_name, weapon_detail_name):
 			editedWeaponDetail.damage = request.form['damage']
 		session.add(editedWeaponDetail)
 		session.commit()
-		return redirect(url_for('showWeaponList', weapon_name = weapon.name))
-	return render_template('editWeaponDetail.html', weapon_detail = editedWeaponDetail, weapon_name = weapon.name)
+		return redirect(url_for('showWeaponList', weapon_name=weapon.name))
+	return render_template(
+		'editWeaponDetail.html',
+		weapon_detail=editedWeaponDetail,
+		weapon_name=weapon.name)
 
 
-#Route to delete weapon detail
-@app.route('/fortnite/<string:weapon_name>/list/<string:weapon_detail_name>/delete', methods = ['GET', 'POST'])
+# Route to delete weapon detail
+
+
+@app.route('/fortnite/<string:weapon_name>/list/<string:weapon_detail_name>/delete', methods=['GET', 'POST'])  # noqa
 def deleteWeaponDetail(weapon_name, weapon_detail_name):
 	if 'username' not in login_session:
 		return redirect('/login')
-	weapon = session.query(Weapon).filter_by(name = weapon_name).one()
-	deletedWeaponDetail = session.query(WeaponDetail).filter_by(name = weapon_detail_name).one()
+	weapon = session.query(Weapon).filter_by(name=weapon_name).one()
+	deletedWeaponDetail = session.query(WeaponDetail).filter_by(
+		name=weapon_detail_name).one()
 	if deletedWeaponDetail.user_id != login_session['user_id']:
-		return "<script>function myFunction() {alert('You are not authorized to delete this weapon.  Are you its creator?');}</script><body onload='myFunction()''>"
+		return "<script>function myFunction() {alert('You are not authorized to delete this weapon.  Are you its creator?');}</script><body onload='myFunction()''>"  # noqa
 	if request.method == 'POST':
 		session.delete(deletedWeaponDetail)
 		session.commit()
-		return redirect(url_for('showWeaponList', weapon_name = weapon.name))
-	return render_template('deleteWeaponDetail.html', weapon_detail = deletedWeaponDetail, weapon_name = weapon.name)
+		return redirect(url_for('showWeaponList', weapon_name=weapon.name))
+	return render_template(
+		'deleteWeaponDetail.html',
+		weapon_detail=deletedWeaponDetail,
+		weapon_name=weapon.name)
 
 
-#Route to homepage JSON format
+# Route to homepage JSON format
+
+
 @app.route('/fortnite/JSON')
 def weaponJSON():
 	weapons = session.query(Weapon).all()
-	return jsonify(Weapons = [weapon.serialize for weapon in weapons])
+	return jsonify(Weapons=[weapon.serialize for weapon in weapons])
 
 
-#Route to weapon list JSON format
+# Route to weapon list JSON format
+
+
 @app.route('/fortnite/<string:weapon_name>/JSON')
 def weaponListJSON(weapon_name):
-	weapon = session.query(Weapon).filter_by(name = weapon_name).one()
-	items = session.query(WeaponDetail).filter_by(weapon_id = weapon.id)
-	return jsonify(Weapon_List = [i.serialize for i in items])
+	weapon = session.query(Weapon).filter_by(name=weapon_name).one()
+	items = session.query(WeaponDetail).filter_by(weapon_id=weapon.id)
+	return jsonify(Weapon_List=[i.serialize for i in items])
 
 
-#Route to individual weapon detail JSON format
-@app.route('/fortnite/<string:weapon_name>/list/<string:weapon_detail_name>/JSON')
+# Route to individual weapon detail JSON format
+
+
+@app.route('/fortnite/<string:weapon_name>/list/<string:weapon_detail_name>/JSON')  # noqa
 def weaponDetailJSON(weapon_name, weapon_detail_name):
-	item = session.query(WeaponDetail).filter_by(name = weapon_detail_name).one()
-	return jsonify(Weapon = item.serialize)
+	item = session.query(WeaponDetail).filter_by(name=weapon_detail_name).one()
+	return jsonify(Weapon=item.serialize)
 
 
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
 	app.debug = True
-	app.run(host = '0.0.0.0', port = 5000)
+	app.run(host='0.0.0.0', port=5000)
--- fortnite_database_setup.py
+++ fortnite_database_setup.py
@@ -1,19 +1,22 @@
-from sqlalchemy import Column,Integer,String,ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
-import random, string
-from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+import random
+import string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)  # noqa
 
 Base = declarative_base()
-secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))  # noqa
 
-#Create User model for database
+# Create User model for database
+
+
 class User(Base):
 	__tablename__ = 'user'
-	id = Column(Integer, primary_key = True)
-	username = Column(String(32), index = True)
+	id = Column(Integer, primary_key=True)
+	username = Column(String(32), index=True)
 	email = Column(String)
 	picture = Column(String)
 	password_hash = Column(String(64))
@@ -25,10 +28,10 @@ class User(Base):
 		return pwd_context.verify(password, self.password_hash)
 
 	def generate_auth_token(self, expiration=600):
-		s = Serializer(secret_key, expires_in = expiration)
-		return s.dumps({'id': self.id })
+		s = Serializer(secret_key, expires_in=expiration)
+		return s.dumps({'id': self.id})
 
-#Static method to verity authentication token
+# Static method to verity authentication token
 	@staticmethod
 	def verify_auth_token(token):
 		s = Serializer(secret_key)
@@ -41,42 +44,46 @@ class User(Base):
 		user_id = data['id']
 		return user_id
 
-#Create Weapon model for database
+# Create Weapon model for database
+
+
 class Weapon(Base):
 	__tablename__ = 'weapon'
-	id = Column(Integer, primary_key = True)
-	name = Column(String(250), nullable = False)
-	user_id = Column(Integer,ForeignKey('user.id'))
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
 	user = relationship(User)
 
 	@property
 	def serialize(self):
 		return {
-		'name' : self.name,
-		'id' : self.id
+			'name': self.name,
+			'id': self.id
 		}
 
-#Create Weapon detail model for database
+# Create Weapon detail model for database
+
+
 class WeaponDetail(Base):
 	__tablename__ = 'weapon_detail'
-	id = Column(Integer, primary_key = True)
-	name = Column(String(250), nullable = False)
-	description = Column(String, nullable = False)
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	description = Column(String, nullable=False)
 	color = Column(String(250))
 	damage = Column(Integer)
-	weapon_id = Column(Integer,ForeignKey('weapon.id'))
+	weapon_id = Column(Integer, ForeignKey('weapon.id'))
 	weapon = relationship(Weapon)
-	user_id = Column(Integer,ForeignKey('user.id'))
+	user_id = Column(Integer, ForeignKey('user.id'))
 	user = relationship(User)
 
 	@property
 	def serialize(self):
 		return {
-		'id' : self.id,
-		'name' : self.name,
-		'description' : self.description,
-		'color' : self.color,
-		'damange' : self.damage
+			'id': self.id,
+			'name': self.name,
+			'description': self.description,
+			'color': self.color,
+			'damage': self.damage
 		}
 
 
@@ -84,5 +91,3 @@ engine = create_engine('sqlite:///fortniteweapondatabase.db')
 
 
 Base.metadata.create_all(engine)
-
-
